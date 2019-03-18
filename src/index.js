4672b31925cecc4335696778f4c7bdd5901cc8b8
antoprokopovich <antoprokopovich@gmail.com>
2019-03-18 11:57:26 +0300

    feat: write a method makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  let obj = {};
+
+  if (currency > 10000) {
+    return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+  } 
+  if (currency <= 0) {
+    return {};
+  } 
+  if (currency >= 50) {
+    obj.H = Math.floor(currency / 50);
+    currency = currency - 50 * obj.H;  
+  } 
+  if (currency >= 25) {
+    obj.Q = Math.floor(currency / 25);
+    currency = currency - 25 * obj.Q;  
+  } 
+  if (currency >= 10) {
+    obj.D = Math.floor(currency / 10);
+    currency = currency - 10 * obj.D;  
+  } 
+  if (currency >= 5) {
+    obj.N = Math.floor(currency / 5);
+    currency = currency - 5 * obj.N;  
+  } 
+  if (currency >= 1) {
+    obj.P = currency;  
+  }
+
+  return obj;
 }
